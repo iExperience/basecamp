a3cebdad64cbd3010fd896be30fddfada85338c9
Salman Ansari <salman.ansari@gmail.com>
2014-06-10 19:22:14 +0100

    added will paginate

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.filter(params[:q])
+    @projects = Project.filter(params[:q]).paginate(:page => params[:page])
   end
 
   # GET /projects/1
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ActiveRecord::Base
 	has_many :items
 
+	self.per_page = 5
+
 	def self.filter(query)
 		query ? Project.where("name LIKE ?", "%#{query}%") : Project.all
 	end
